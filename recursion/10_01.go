3dde264124569d9a30b75f093bede1a8093ef14b
lyeka <augustr2017@163.com>
2021-02-11 21:36:57 +0800

    add 09 & 10_01

---
+++ recursion/10_01.go
@@ -0,0 +1,13 @@
+package recursion
+
+
+func fib(n int) int {
+	x, y := 0, 1
+	sum := x
+	for i:=0; i<n; i++{
+		sum = (x + y) % 1000000007
+		x = y
+		y = sum
+	}
+	return x
+}
---
+++ stack_and_queue/09.go
@@ -0,0 +1,52 @@
+package stack_and_queue
+
+type CQueue struct {
+	s1 *Stack
+	s2 *Stack
+}
+
+
+func Constructor() CQueue {
+	return CQueue{s1: &Stack{}, s2: &Stack{}}
+}
+
+type Stack struct {
+	d []int
+}
+
+func (s *Stack) Add(v int) {
+	s.d = append(s.d, v)
+}
+
+func (s *Stack) Pop() int {
+	if s.len() <= 0 {
+		return -1
+	}
+	res := s.d[len(s.d)-1]
+	s.d = s.d[:len(s.d)-1]
+	return res
+}
+
+func (s *Stack) len() int {
+	return len(s.d)
+}
+
+func (this *CQueue) AppendTail(value int)  {
+	this.s1.Add(value)
+}
+
+
+func (this *CQueue) DeleteHead() int {
+	if this.s1.len() == 0 && this.s2.len() == 0 {
+		return -1
+	}
+
+	if this.s2.len() > 0 {
+		return this.s2.Pop()
+	}
+	for this.s1.len() > 0 {
+		this.s2.Add(this.s1.Pop())
+	}
+	return this.s2.Pop()
+}
+
